285ecec02fd148bb58740f917142fc34750a1f91
Piotr Domian <domianpiotr111@gmail.com>
2021-04-20 18:15:37 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function createList() {
-  // Zaimplementuj zadanie zgodnie z wytycznymi
+  var ul = document.createElement('ul');
+  ul.setAttribute("id", "list");
+	for (var i = 0; i<3; i++)
+  {
+    var li = document.createElement('li');
+    li.innerHTML = "text";
+    ul.appendChild(li);
+  }
+  document.body.appendChild(ul);
+  return ul;
 };
 
 module.exports = createList;
